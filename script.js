73db6b7c489db083b22ea360bb1e77642260482b
devonnn <cbark1313@gmail.com>
2026-05-24 11:42:00 -0400

    v1

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.content');
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+
+    });
+
+},{
+    threshold:0.3
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
